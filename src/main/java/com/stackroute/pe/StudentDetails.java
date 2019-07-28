607ee1afefa4445da2a55a28504226b70fdbc05c
sneha <sneha.meshram@cgi.com>
2019-07-28 17:07:38 +0530

    update StudentDetails class

--- src/main/java/com/stackroute/pe/StudentDetails.java
+++ src/main/java/com/stackroute/pe/StudentDetails.java
@@ -1,5 +1,68 @@
 package com.stackroute.pe;
 
+import java.util.Comparator;
+
 public class StudentDetails {
 
+    private int id;
+    private String name;
+    private int age;
+
+    //constructor
+    public StudentDetails(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
+
+class StudentSort implements Comparator<StudentDetails> {
+
+    @Override
+    public int compare(StudentDetails list1, StudentDetails list2) {
+        if (list1.getAge() == list2.getAge()) {
+            if (list1.getName().equals(list2.getName()))
+                return list1.getId() - list2.getId();
+            else
+                return list2.getName().compareTo(list1.getName());
+        } else {
+            return
+                    list2.getAge() - list1.getAge();
+        }
+    }
 }
--- src/test/java/com/stackroute/pe/StudentDetailsTest.java
+++ src/test/java/com/stackroute/pe/StudentDetailsTest.java
@@ -3,6 +3,8 @@ package com.stackroute.pe;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,22 +15,21 @@ public class StudentDetailsTest {
     @Test
 
     public void givenClassShouldReturnTheStudentDetails(){
-        ArrayList<StudentDetails> inputStudent=new ArrayList();
+        List<StudentDetails> inputStudent=new ArrayList();
+
         StudentDetails detail1=new StudentDetails(1,"stella",23);
-        StudentDetails detail2=new StudentDetails(2,"julie",23);
-        StudentDetails detail3=new StudentDetails(1,"rosie",23);
+        StudentDetails detail2=new StudentDetails(2,"julie",33);
+        StudentDetails detail3=new StudentDetails(3,"rosie",45);
+        StudentDetails detail4=new StudentDetails(4,"julie",17);
 
         inputStudent.add(detail1);
         inputStudent.add(detail2);
         inputStudent.add(detail3);
+        inputStudent.add(detail4);
 
-        String expectedOutput={}
-
-
-
-
-
-
+        Collections.sort(inputStudent,new StudentSort());
+        String expected="[Student{id=1, name='stella', age=23}, Student{id=2, name='julie', age=33}, Student{id=3, name='rosie', age=45}}]";
+        assertEquals(expected, inputStudent.toString());
     }
 
 }
